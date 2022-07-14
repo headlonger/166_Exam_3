01160f7efbbe6a0834a6654c26f689f521d451dc
Yury Gaidukevich <headlonger@gmail.com>
2022-07-14 11:29:06 +0300

    Task1.py delete trash

--- Task1.py
+++ Task1.py
@@ -11,22 +11,6 @@ cardnum = ''
 cvc = ''
 pin = ''
 
-# while cardnum != 16:
-#     cardnum = input('Введите номер карты: ')
-#
-#     if len(cardnum) == 16 and cardnum.isdigit():
-#         cvc = input('Введите CVC: ')
-#         if len(cvc) == 3 and cvc.isdigit():
-#             pin = input('Введите PIN: ')
-#         else:
-#             print('Вы ввели неверный cvc карты!')
-#             if len(pin) == 4 and pin.isdigit():
-#                 card(cardnum, cvc, pin)
-#             else:
-#                 print('Вы ввели неверный pin карты!')
-#     else:
-#         print('Вы ввели неверный номер карты!')
-
 while len(cardnum) != 16:
     cardnum = input('Введите номер карты: ')
 
